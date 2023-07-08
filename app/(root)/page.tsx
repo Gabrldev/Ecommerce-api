d3db1ae4fa93d32b503e36f5fb0b0f9e0e4e20fa
Gabriel <newaydsg@gmail.com>
2023-07-08 17:16:58 -0400

    start app

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from '@/components/ui/Button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Login</Button>
+      </div>
+  )
+}
